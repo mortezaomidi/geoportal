5b0f29cae6dd79bbbdf38d1de98c180895dd03fc
mortezaomidi <omidiporm@gmail.com>
2021-11-25 07:13:55 +0330

    organization model

--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from django.urls import reverse
 
-# Create your models here.
+
+class Organization(models.Model):
+    '''
+    There are many organization that publish their's KDWS services.
+    '''
+    name = models.CharField(_('title'), max_length=100)
+    url = models.URLField(_('url'), max_length=200)
+    description = models.TextField(_('description'))
+    logo = models.FileField(_('url'), upload_to='uploads/', default='default_logo.png')
+    
+
+    class Meta:
+        verbose_name = _("Organization")
+        verbose_name_plural = _("Organizations")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("organization_detail", kwargs={"pk": self.pk})
+
