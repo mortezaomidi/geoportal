71ae94fc31440011e37c258c25f8e11662ba8c99
mortezaomidi <omidiporm@gmail.com>
2021-11-25 09:19:02 +0330

    adding home page

---
+++ frontend/src/components/Drawer.vue
@@ -0,0 +1,39 @@
+<template>
+<div>      
+    <v-app-bar app color="teal" dense>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">Knowledge-based Geoportal</v-toolbar-title> <v-spacer></v-spacer>
+    </v-app-bar> 
+    <v-navigation-drawer v-model="drawer" app>
+        <v-list-item>
+            <v-list-item-avatar>
+                <v-img src="../assets/img/morteza.jpg"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+                <v-list-item-title>Morteza Omidipoor</v-list-item-title>
+                <v-list-item-subtitle>PhD student</v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+            <v-list-item link @click="$router.push({name: 'Home'})">
+                <v-list-item-icon> <v-icon>mdi-home</v-icon> </v-list-item-icon>
+                <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item link @click="$router.push({name: 'Logout'})">
+                <v-list-item-icon> <v-icon color="red darken-2">mdi-logout</v-icon> </v-list-item-icon>
+                <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Drawer',
+    data: () => ({
+      drawer: false
+    }),    
+}
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <Drawer/>      
+      <v-main>
+        <h1>Welcome!</h1>
+      </v-main>       
+  </div>
+</template>
+
+<script>
+import Drawer from '../components/Drawer.vue'
+export default {
+  name: 'Home',
+  components: {
+    Drawer,
+  }
+}
+</script>
